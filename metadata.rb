8cc1f1e8becae0de87e92d9f06775fb0ab5ea1e6
keviny22 <kevin_young@intuit.com>
2013-03-21 15:08:22 -0700

    Added metadata dependancies back

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,10 @@ maintainer_email "kevin_young@intuit.com"
 description      "Installs Effin Package Manager"
 version          "0.1.0"
 
+depends          "ruby"
+depends          "rpm-build"
+depends          "build-essential"
+
 %w{ redhat }.each do |os|
   supports os
 end
