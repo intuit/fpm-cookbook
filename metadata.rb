ca12e7fa256b6f620b7ecf0cbb6fc2c729525474
keviny22 <kevin_young@intuit.com>
2013-03-21 15:01:51 -0700

    Removed ruby dependancy

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,7 @@
 maintainer       "Intuit, Inc."
-maintainer_email "brett_weaver@intuit.com"
+maintainer_email "kevin_young@intuit.com"
 description      "Installs Effin Package Manager"
 version          "0.1.0"
-depends          "ruby"
-depends          "rpm-build"
-depends          "build-essential"
 
 %w{ redhat }.each do |os|
   supports os
